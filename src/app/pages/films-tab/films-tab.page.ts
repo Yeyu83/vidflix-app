a7db534337132c509b54dbdcca59536958d3477b
yeyu83 <chemabravoh@hotmail.com>
2021-10-03 09:56:21 +0200

    feat: films tab infinite scroll working

--- src/app/pages/films-tab/films-tab.page.ts
+++ src/app/pages/films-tab/films-tab.page.ts
@@ -3,7 +3,7 @@ import { Film } from '../../interfaces/film.interface';
 import { FilmsService } from '../../services/films.service';
 import { RowsResponse } from '../../interfaces/rows.response.interface';
 import { FilmsFilter } from 'src/app/classes/film-filters.class';
-import { IonContent } from '@ionic/angular';
+import { IonContent, IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-films-tab',
@@ -13,22 +13,34 @@ import { IonContent } from '@ionic/angular';
 export class FilmsTabPage {
   @ViewChild(IonContent, { static: false }) content: IonContent;
 
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+
   public films: Film[] = [];
 
   public filters = new FilmsFilter();
+
+  private page = 1;
 
   constructor(
     private readonly filmsService: FilmsService
   ) { }
 
   ionViewWillEnter(): void {
+    this.infiniteScroll.disabled = false;
     this.content.scrollToTop();
     this.getFilms();
   }
 
   public getFilms(): void {
-    this.filmsService.getFilmsByFilter(this.filters).subscribe((res: RowsResponse<Film>) => {
-      this.films = [...this.films, ...res.rows];
+    const filters = { ...this.filters, page: String(this.page) };
+    this.filmsService.getFilmsByFilter(filters).subscribe((res: RowsResponse<Film>) => {
+      this.infiniteScroll.complete();
+      if (res.rows.length) {
+        this.films = [...this.films, ...res.rows];
+        this.page += 1;
+      } else {
+        this.infiniteScroll.disabled = true;
+      }
     });
   }
 }
